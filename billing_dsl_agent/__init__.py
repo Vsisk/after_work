2b6d4ba4f9b4dfb2b09199814a1b3f1ea0f6656e
Vsisk <90754922+Vsisk@users.noreply.github.com>
2026-03-23 16:44:45 +0800

    Group public API exports and classify loader methods

--- billing_dsl_agent/__init__.py
+++ billing_dsl_agent/__init__.py
@@ -1,16 +1,33 @@
 from billing_dsl_agent.agent_entry import DSLAgent, generate_dsl
+from billing_dsl_agent.bo_loader import load_bo_registry_from_file, load_bo_registry_from_json
+from billing_dsl_agent.context_loader import (
+    build_context_path_map,
+    load_context_registry_from_file,
+    load_context_registry_from_json,
+)
 from billing_dsl_agent.llm_planner import LLMPlanner, StubOpenAIClient
 from billing_dsl_agent.models import GenerateDSLRequest, GenerateDSLResponse, NodeDef
 from billing_dsl_agent.resource_manager import ResourceManager, build_candidate_prompt_payload
 
 __all__ = [
+    # Agent entry
     "DSLAgent",
     "generate_dsl",
+    # Planner
     "LLMPlanner",
     "StubOpenAIClient",
+    # Request / Response
     "GenerateDSLRequest",
     "GenerateDSLResponse",
     "NodeDef",
+    # Resource manager
     "ResourceManager",
     "build_candidate_prompt_payload",
+    # BO loader
+    "load_bo_registry_from_json",
+    "load_bo_registry_from_file",
+    # Context loader
+    "load_context_registry_from_json",
+    "load_context_registry_from_file",
+    "build_context_path_map",
 ]
