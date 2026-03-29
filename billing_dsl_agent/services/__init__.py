86b8308b4119e995cb0c4f5bdec3590da547c78a
Vsisk <wzyvsss@gmail.com>
2026-03-30 00:03:34 +0800

    统一模型调用

--- billing_dsl_agent/services/__init__.py
+++ billing_dsl_agent/services/__init__.py
@@ -1,15 +1,18 @@
-from billing_dsl_agent.services.llm_client import LLMClientError, OpenAILLMClient, extract_param
-from billing_dsl_agent.services.llm_post_processor import post_process_response
+from billing_dsl_agent.services.llm_client import LLMClientError, OpenAILLMClient, RawLLMInvocation, extract_param
+from billing_dsl_agent.services.llm_post_processor import extract_response_text, post_process_response
 from billing_dsl_agent.services.llm_service import PromptDrivenLLMService
 from billing_dsl_agent.services.prompt_manager import PromptManager, PromptManagerError
+from billing_dsl_agent.services.structured_llm_executor import StructuredLLMExecutor
 
 __all__ = [
     "LLMClientError",
     "OpenAILLMClient",
+    "RawLLMInvocation",
     "PromptDrivenLLMService",
     "PromptManager",
     "PromptManagerError",
+    "StructuredLLMExecutor",
+    "extract_response_text",
     "extract_param",
     "post_process_response",
 ]
-
